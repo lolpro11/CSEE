b7cba8a78fca590840e906e2a979b5c8d16dcd2b
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-07-02 01:27:43 -0700

    unwrap

--- src/html.rs
+++ src/html.rs
@@ -26,16 +26,17 @@ async fn main() {
 
     let mut hub = Classroom::new(hyper::Client::builder().build(hyper_rustls::HttpsConnectorBuilder::new().with_native_roots().https_or_http().enable_http1().build()), auth);
     let courses = hub.courses();
-    let (response, ListCoursesResponse { courses: Some(courses), .. }): (Response<Body>, ListCoursesResponse) = (match courses.list().page_size(1).doit().await {
-        Ok(response) => response,
-        Err(error) => {
-            eprintln!("Error: {}", error);
-            unreachable!()
-        }
-    }) else { unreachable!() };
+    let response: (Response<Body>, ListCoursesResponse) = courses.list().page_size(1).doit().await.unwrap();
 
-    for course in courses {
+    /*for course in courses {
         println!("Courses: {}", (course.id.unwrap()));
+    }*/
+
+    for course in response.1.courses.unwrap() {
+        println!("{}", match course.name {
+            Some(name) => name,
+            None => "No name found!".to_string()
+        });
     }
     //let r = hub.courses().course_work_student_submissions_list(course.id.unwrap()).doit().await
     //let r = hub.courses().course_work_list(course.id.unwrap()).doit().await
@@ -43,3 +44,9 @@ async fn main() {
     //let r = hub.courses().teachers_list(course.id.unwrap()).doit().await
     //let r = hub.courses().topics_list(course.id.unwrap()).doit().await
 }
+
+//str - Stack allocated, not mutable (usually). have to know size at compile time.
+
+//String - Heap allocated, can be mutable (with 1 referance only OR Rust mem lock like RWlock), can grow or shrink size.
+
+//char - single character, including unicode, can be mutable

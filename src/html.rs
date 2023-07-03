2a54253555f66b9d1a91de80ccc79c13400e2874
lolpro11 <lolpro11lolx@gmail.com>
2023-07-03 14:39:38 -0700

    add work

--- src/html.rs
+++ src/html.rs
@@ -49,21 +49,13 @@ async fn main() {
                                 let form_link = material.form.unwrap().form_url.unwrap();
                                 println!("form: {}", form_link);
                             };
-                            match material.drive_file {
-                                Some(drive_file) => {
-                                    match drive_file.drive_file {
-                                        Some(drive_file) => {
-                                            if drive_file.title.is_some() {
-                                                println!("{}", drive_file.title.unwrap());
-                                            };
-                                            if drive_file.alternate_link.is_some() {
-                                                println!("{}", drive_file.alternate_link.unwrap());
-                                            };
-                                        }
-                                        None => ()
-                                    }
-                                }
-                                None => ()
+                            if material.drive_file.clone().is_some() && material.drive_file.clone().unwrap().drive_file.is_some() {
+                                if material.drive_file.clone().unwrap().drive_file.unwrap().title.is_some() {
+                                    println!("{}", material.drive_file.clone().unwrap().drive_file.unwrap().title.unwrap());
+                                };
+                                if material.drive_file.clone().unwrap().drive_file.unwrap().alternate_link.is_some() {
+                                    println!("{}", material.drive_file.clone().unwrap().drive_file.unwrap().alternate_link.unwrap());
+                                };
                             }
                         }
                     }
@@ -76,14 +68,78 @@ async fn main() {
                 println!("");
             }
         } else {
-            println!("No announcements\n")
+            println!("No announcements\n");
         }
-        /*let course_work_student_submission_list: (Response<Body>, ListStudentSubmissionsResponse) = courses.course_work_student_submissions_list(course_id: &the_id).doit().await.unwrap();
-        let course_work: (Response<Body>, ListCourseWorkResponse) = courses.course_work_list(course_id: &the_id).doit().await.unwrap();
-        let course_materials: (Response<Body>, ListCourseWorkMaterialResponse) = courses.course_work_materials_list(course_id: &the_id).doit().await.unwrap();
+        let course_work: (Response<Body>, ListCourseWorkResponse) = courses.course_work_list(&the_id).doit().await.unwrap();
+        if course_work.1.course_work.is_some() {
+            let course_work_list = course_work.1.course_work.unwrap();
+            for course in course_work_list {
+                if course.assignee_mode.is_none() {
+                    if course.alternate_link.is_some() {
+                        println!("Link to work {}", course.alternate_link.unwrap());
+                    } else {
+                        println!("No link");
+                    }
+                    println!("time made: {:#?}", course.creation_time.unwrap());
+                    println!("Author id: {}", match course.creator_user_id {
+                        Some(creator_user_id) => creator_user_id,
+                        None => "Unknown Author id".to_string()
+                    });
+                    println!("last updated: {:#?}", match course.update_time {
+                        Some(update_time) => update_time,
+                        None => course.creation_time.unwrap()
+                    });
+                    if course.assignment.is_some() && course.assignment.clone().unwrap().student_work_folder.is_some() {
+                        if course.assignment.clone().unwrap().student_work_folder.unwrap().alternate_link.is_some() {
+                            println!("Work folder: {}", course.assignment.clone().unwrap().student_work_folder.unwrap().alternate_link.is_some());
+                        };
+                        if course.assignment.clone().unwrap().student_work_folder.unwrap().title.is_some() {
+                            println!("Name of folder: {}", course.assignment.clone().unwrap().student_work_folder.unwrap().title.is_some());
+                        };
+                    }
+                }
+                println!("assignment: {}", match course.description {
+                    Some(description) => description,
+                    None => "None".to_string()
+                });
+                let mut date_due: String = "".to_string();
+                if course.due_date.is_some() {
+                    match course.due_date.clone().unwrap().year {
+                        Some(year) => date_due.push_str(&year.to_string()),
+                        None => ()
+                    }
+                    date_due.push_str("-");
+                    match course.due_date.clone().unwrap().day {
+                        Some(day) => date_due.push_str(&day.to_string()),
+                        None => ()
+                    }
+                    date_due.push_str("-");
+                    match course.due_date.clone().unwrap().month {
+                        Some(month) => date_due.push_str(&month.to_string()),
+                        None => ()
+                    }
+                }
+                if course.due_time.is_some() {
+                    date_due.push_str(" ");
+                    match course.due_time.clone().unwrap().hours {
+                        Some(hours) => date_due.push_str(&hours.to_string()),
+                        None => ()
+                    }
+                    date_due.push_str(":");
+                    match course.due_time.clone().unwrap().minutes {
+                        Some(minutes) => date_due.push_str(&minutes.to_string()),
+                        None => ()
+                    }
+                    println!("Due at {}", date_due);
+                } 
+                println!(" ");
+            }
+        }
+        /*let course_materials: (Response<Body>, ListCourseWorkMaterialResponse) = courses.course_work_materials_list(course_id: &the_id).doit().await.unwrap();
         let teachers: (Response<Body>, ListTeachersResponse) = courses.teachers_list(course_id: &the_id).doit().await.unwrap();
         let topics: (Response<Body>, ListTopicResponse) = courses.topics_list(course_id: &the_id).doit().await*/
-        //println!("Courses: {:#?}", course_announcements);
+        //let course_work_student_submission_list: (Response<Body>, ListStudentSubmissionsResponse) = courses.course_work_student_submissions_list(course_id: &the_id).doit().await.unwrap();
+
     }
 }
 

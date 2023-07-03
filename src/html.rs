8a481d8bd83804869c330d47b1177d862448b2da
lolpro11 <lolpro11lolx@gmail.com>
2023-07-03 16:34:42 -0700

    add teachers

--- src/html.rs
+++ src/html.rs
@@ -1,5 +1,5 @@
 extern crate google_classroom1 as classroom1;
-use classroom1::{api::{ListAnnouncementsResponse, ListStudentSubmissionsResponse, ListCourseWorkResponse, ListCourseWorkMaterialResponse, ListTeachersResponse, ListTopicResponse, Material}};
+use classroom1::{api::{ListAnnouncementsResponse, ListStudentSubmissionsResponse, ListCourseWorkResponse, ListCourseWorkMaterialResponse, ListTeachersResponse, ListTopicResponse, Material, Teacher}};
 use classroom1::{Classroom, hyper, hyper_rustls};
 use google_classroom1::api::ListCoursesResponse;
 use hyper::Body;
@@ -37,6 +37,9 @@ async fn main() {
                     None => "No text".to_string()
                 });
                 println!("time made: {:#?}", announcement.creation_time.unwrap());
+                if announcement.scheduled_time.is_some() {
+                    println!("time published: {}", announcement.scheduled_time.unwrap());
+                }
                 println!("Author id: {}", match announcement.creator_user_id {
                     Some(creator_user_id) => creator_user_id,
                     None => "Unknown Author id".to_string()
@@ -84,6 +87,9 @@ async fn main() {
                         println!("No link");
                     }
                     println!("time made: {:#?}", course.creation_time.unwrap());
+                    if course.scheduled_time.is_some() {
+                        println!("time published: {}", course.scheduled_time.unwrap());
+                    }
                     println!("Author id: {}", match course.creator_user_id {
                         Some(creator_user_id) => creator_user_id,
                         None => "Unknown Author id".to_string()
@@ -184,30 +190,55 @@ async fn main() {
                 println!(" ");
             }
         }
-        let course_materials: (Response<Body>, ListCourseWorkMaterialResponse) = courses.course_work_materials_list(course_id: &the_id).doit().await.unwrap();
+        let course_materials: (Response<Body>, ListCourseWorkMaterialResponse) = courses.course_work_materials_list(&the_id).doit().await.unwrap();
         if course_materials.1.course_work_material.is_some() {
             for course_mats in course_materials.1.course_work_material.unwrap() {
-                if course_mats.alternate_link.is_some() {
-                    println!("Link to work {}", course_mats.alternate_link.unwrap());
-                } else {
-                    println!("No link");
+                if course_mats.assignee_mode.is_none() {
+                    if course_mats.alternate_link.is_some() {
+                        println!("Link to work {}", course_mats.alternate_link.unwrap());
+                    } else {
+                        println!("No link");
+                    }
+                    println!("time made: {:#?}", course_mats.creation_time.unwrap());
+                    if course_mats.scheduled_time.is_some() {
+                        println!("time published: {}", course_mats.scheduled_time.unwrap());
+                    }
+                    println!("Author id: {}", match course_mats.creator_user_id {
+                        Some(creator_user_id) => creator_user_id,
+                        None => "Unknown Author id".to_string()
+                    });
+                    println!("Material: {}", match course_mats.description {
+                        Some(description) => description,
+                        None => "None".to_string()
+                    });
+                    if course_mats.id.is_some() {
+                        println!("Id: {}", course_mats.id.unwrap());
+                    }
+                    if course_mats.topic_id.is_some() {
+                        println!("In Topic Id: {}", course_mats.topic_id.unwrap());
+                    }
+                    println!("last updated: {:#?}", match course_mats.update_time {
+                        Some(update_time) => update_time,
+                        None => course_mats.creation_time.unwrap()
+                    });
+                    println!(" ");
                 }
-                println!("time made: {:#?}", course.creation_time.unwrap());
-                println!("Author id: {}", match course_mats.creator_user_id {
-                    Some(creator_user_id) => creator_user_id,
-                    None => "Unknown Author id".to_string()
-                });
-                println!("Material: {}", match course_mats.description {
-                    Some(description) => description,
-                    None => "None".to_string()
-                });
-                if course_mats.id.is_some() {
-                    println!("Topic: {}", course_mats.id.unwrap());
-                }
-                
             }
         }
-        //let teachers: (Response<Body>, ListTeachersResponse) = courses.teachers_list(course_id: &the_id).doit().await.unwrap();
+        let teachers: (Response<Body>, ListTeachersResponse) = courses.teachers_list(&the_id).doit().await.unwrap();
+        if teachers.1.teachers.is_some() {
+            for teacher in teachers.1.teachers.unwrap() {
+                if teacher.user_id.is_some() {
+                    println!("User Id: {}", teacher.user_id.unwrap());
+                }
+                if teacher.profile.is_some() {
+                    teacher_profile = "".to_string();
+                    if teacher.profile.clone().unwrap().email_address.is_some() {
+                        
+                    }
+                }
+            }
+        }
         //let topics: (Response<Body>, ListTopicResponse) = courses.topics_list(course_id: &the_id).doit().await*/
         //let course_work_student_submission_list: (Response<Body>, ListStudentSubmissionsResponse) = courses.course_work_student_submissions_list(course_id: &the_id).doit().await.unwrap();
 

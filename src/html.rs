671a72e10b02be8a4af24d90e92d64c59fee2487
lolpro11 <lolpro11lolx@gmail.com>
2023-07-03 00:09:49 -0700

    finish announcements

--- src/html.rs
+++ src/html.rs
@@ -1,6 +1,6 @@
 extern crate google_classroom1 as classroom1;
 use classroom1::{api::{ListAnnouncementsResponse, ListStudentSubmissionsResponse, ListCourseWorkResponse, ListCourseWorkMaterialResponse, ListTeachersResponse, ListTopicResponse, Material}};
-use classroom1::{Classroom, oauth2, hyper, hyper_rustls, chrono, FieldMask};
+use classroom1::{Classroom, hyper, hyper_rustls};
 use google_classroom1::api::ListCoursesResponse;
 use hyper::Body;
 use hyper::Response;
@@ -21,53 +21,68 @@ async fn main() {
 
     let hub = Classroom::new(hyper::Client::builder().build(hyper_rustls::HttpsConnectorBuilder::new().with_native_roots().https_or_http().enable_http1().build()), auth);
     let courses = hub.courses();
-    let response: (Response<Body>, ListCoursesResponse) = courses.list().page_size(1).doit().await.unwrap();
+    let response: (Response<Body>, ListCoursesResponse) = courses.list().page_size(100).doit().await.unwrap();
 
     for course in response.1.courses.unwrap() {
         let the_id = course.id.unwrap().clone();
+        println!("Course: {}, {}", course.name.unwrap(), the_id);
         let course_announcements: (Response<Body>, ListAnnouncementsResponse) = courses.announcements_list(&the_id).doit().await.unwrap();
-        for announcement in course_announcements.1.announcements.unwrap() {
-            println!("announcement: {}", match announcement.text {
-                Some(text) => text,
-                None => "No text".to_string()
-            });
-            println!("time made: {:#?}", announcement.creation_time.unwrap());
-            println!("Author id: {}", match announcement.creator_user_id {
-                Some(creator_user_id) => creator_user_id,
-                None => "Unknown Author id".to_string()
-            });
-            println!("id: {}", match announcement.id {
-                Some(id) => id,
-                None => "Unknown id".to_string()
-            });
-            match announcement.materials {
-                Some(materials) => {
-                    for material in materials {
-                        match material.form {
-                            Some(forms) => {
-                                match forms.form_url {
-                                    Some(form_url) => println!("form: {}", form_url),
-                                    None => ()
+        if course_announcements.1.announcements.is_some() {
+            for announcement in course_announcements.1.announcements.unwrap() {
+                println!("announcement: {}", match announcement.text {
+                    Some(text) => text,
+                    None => "No text".to_string()
+                });
+                println!("time made: {:#?}", announcement.creation_time.unwrap());
+                println!("Author id: {}", match announcement.creator_user_id {
+                    Some(creator_user_id) => creator_user_id,
+                    None => "Unknown Author id".to_string()
+                });
+                println!("id: {}", match announcement.id {
+                    Some(id) => id,
+                    None => "Unknown id".to_string()
+                });
+                match announcement.materials {
+                    Some(materials) => {
+                        for material in materials {
+                            match material.form {
+                                Some(forms) => {
+                                    if forms.form_url.is_some() {
+                                        println!("form: {}", forms.form_url.unwrap());
+                                    }
                                 }
+                                None => ()
                             }
-                            None => ()
-                        }
-                        match material.drive_file {
-                            Some(drive_file) => {
-                                match drive_file.drive_file {
-                                    Some(drive_file) => match drive_file.alternate_link {
-                                        Some(alternate_link) => println!("file: {}", alternate_link),
+                            match material.drive_file {
+                                Some(drive_file) => {
+                                    match drive_file.drive_file {
+                                        Some(drive_file) => {
+                                            match drive_file.alternate_link {
+                                                Some(alternate_link) => println!("{}", alternate_link),
+                                                None => (),
+                                            };
+                                            match drive_file.title {
+                                                Some(title) => println!("{}", title),
+                                                None => (),
+                                            };
+                                        }
                                         None => ()
                                     }
-                                    None => ()
                                 }
+                                None => ()
                             }
-                            None => ()
                         }
                     }
+                    None => println!("No mats")
                 }
-                None => println!("No mats")
+                println!("last updated: {:#?}", match announcement.update_time {
+                    Some(update_time) => update_time,
+                    None => announcement.creation_time.unwrap()
+                });
+                println!("");
             }
+        } else {
+            println!("No announcements\n")
         }
         /*let course_work_student_submission_list: (Response<Body>, ListStudentSubmissionsResponse) = courses.course_work_student_submissions_list(course_id: &the_id).doit().await.unwrap();
         let course_work: (Response<Body>, ListCourseWorkResponse) = courses.course_work_list(course_id: &the_id).doit().await.unwrap();
@@ -75,7 +90,6 @@ async fn main() {
         let teachers: (Response<Body>, ListTeachersResponse) = courses.teachers_list(course_id: &the_id).doit().await.unwrap();
         let topics: (Response<Body>, ListTopicResponse) = courses.topics_list(course_id: &the_id).doit().await*/
         //println!("Courses: {:#?}", course_announcements);
-        println!("Courses: {}", the_id);
     }
 }
 

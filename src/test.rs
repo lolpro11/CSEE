aea47c7b792b76e8a970ec160513f79503d81a34
lolpro11 <lolpro11lolx@gmail.com>
2023-08-13 20:40:36 -0700

    add concurrency to test.rs

--- src/test.rs
+++ src/test.rs
@@ -13,14 +13,29 @@ use serde::{Deserialize, Serialize};
 use tera::Tera;
 use std::fs::File;
 use std::io::{self, Read, Write};
-use classroom1::api::{ListAnnouncementsResponse, ListCoursesResponse, ListCourseWorkResponse, ListCourseWorkMaterialResponse, ListTeachersResponse, ListTopicResponse};
+use std::time::Instant;
+use classroom1::api::{ListCoursesResponse, Announcement, CourseWork, CourseWorkMaterial, Teacher, Topic};
 use classroom1::{Classroom, hyper, hyper_rustls, chrono};
 use serde_json::Value;
 use tera::Context;
 use tokio::task;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use tokio::runtime::Builder;
+use std::sync::{Arc, Mutex, mpsc};
+use tokio::runtime::{Builder, Runtime};
+
+#[derive(Clone)]
+struct CourseContent {
+    id: Option<String>,
+    course_announcements: Option<Vec<Announcement>>,
+    course_work: Option<Vec<CourseWork>>,
+    course_materials: Option<Vec<CourseWorkMaterial>>,
+    name: Option<String>,
+    teachers: Option<Vec<Teacher>>,
+    topics: Option<Vec<Topic>>,
+    tera: Tera,
+}
+
+
 // AuthCallbackParams struct for deserialization of query parameters
 #[derive(Deserialize)]
 struct AuthCallbackParams {
@@ -116,22 +131,22 @@ async fn login() -> HttpResponse {
 
 async fn fetch_classroom_data(auth_secret: ApplicationSecret) -> Result<(), MyError> {
     // Create the OAuth2 authenticator using the ApplicationSecret
-    let authenticator = InstalledFlowAuthenticator::builder(auth_secret, InstalledFlowReturnMethod::HTTPRedirect)
+    let auth = InstalledFlowAuthenticator::builder(auth_secret, InstalledFlowReturnMethod::HTTPRedirect)
         .persist_tokens_to_disk("tokens.json")
         .build()
         .await
         .expect("Failed to build InstalledFlowAuthenticator");
 
-    let hub = Classroom::new(hyper::Client::builder().build(hyper_rustls::HttpsConnectorBuilder::new().with_native_roots().https_or_http().enable_http1().build()), authenticator);
+    let hub = Classroom::new(hyper::Client::builder().build(hyper_rustls::HttpsConnectorBuilder::new().with_native_roots().https_or_http().enable_http1().build()), auth);
     let courses = hub.courses();
     let response: (Response<Body>, ListCoursesResponse) = courses.list().page_size(100).doit().await.unwrap();
-    
-    println!("{:?}", response);
+
     let mut tera = Tera::new("../templates/**/*.html").unwrap();
     let mut context = Context::new();
     let mut buffer = Vec::new();
     let course_list = response.1.courses.clone().unwrap();
     tera.add_template_file("templates/courses.html", Some("course_list")).unwrap();
+    tera.add_template_file("templates/course.html", Some("course")).unwrap();
     context.insert("courses", &course_list);
     tera.render_to("course_list", &context, &mut buffer).unwrap();
     let mut file = File::create("html/courses.html").expect("Failed to create file");
@@ -167,184 +182,121 @@ async fn fetch_classroom_data(auth_secret: ApplicationSecret) -> Result<(), MyEr
             Err(tera::Error::msg("No 'id' argument provided"))
         }
     });
-    
-    
-    file.write_all(&buffer).expect("Failed to write to file");
-    buffer = Vec::new();
-    for course in response.1.courses.unwrap() {
-        let the_id = course.clone().id.unwrap();
-        println!("Course: {}, {}", course.name.clone().unwrap(), the_id);
-        tera.add_template_file("templates/course.html", Some("course")).unwrap();
-        context.insert("course", &course);
-        let course_announcements: (Response<Body>, ListAnnouncementsResponse) = courses.announcements_list(&the_id).doit().await.unwrap();
-        if course_announcements.1.announcements.is_some() {
-            context.insert("course_announcements", &course_announcements.1.announcements.clone().unwrap());
+
+    async fn check_url(url: String) -> bool {
+        match reqwest::get(&url).await {
+            Ok(response) => response.status() == reqwest::StatusCode::OK,
+            Err(_) => false,
         }
-        let course_work: (Response<Body>, ListCourseWorkResponse) = courses.course_work_list(&the_id).doit().await.unwrap();
-        if course_work.1.course_work.is_some() {
-            let course_work_list = course_work.1.course_work.unwrap();
-            for course in course_work_list {
-                if course.assignee_mode.is_none() {
-                    if course.alternate_link.is_some() {
-                        println!("Link to work {}", course.alternate_link.unwrap());
-                    } else {
-                        println!("No link");
-                    }
-                    println!("time made: {:#?}", course.creation_time.unwrap());
-                    if course.scheduled_time.is_some() {
-                        println!("time published: {}", course.scheduled_time.unwrap());
-                    }
-                    println!("Author id: {}", match course.creator_user_id {
-                        Some(creator_user_id) => creator_user_id,
-                        None => "Unknown Author id".to_string()
-                    });
-                    println!("last updated: {:#?}", match course.update_time {
-                        Some(update_time) => update_time,
-                        None => course.creation_time.unwrap()
-                    });
-                    if course.assignment.is_some() && course.assignment.clone().unwrap().student_work_folder.is_some() {
-                        if course.assignment.clone().unwrap().student_work_folder.unwrap().alternate_link.is_some() {
-                            println!("Work folder: {}", course.assignment.clone().unwrap().student_work_folder.unwrap().alternate_link.is_some());
-                        };
-                        if course.assignment.clone().unwrap().student_work_folder.unwrap().title.is_some() {
-                            println!("Name of folder: {}", course.assignment.clone().unwrap().student_work_folder.unwrap().title.is_some());
-                        };
-                    }
-                }
-                println!("assignment: {}", match course.description {
-                    Some(description) => description,
-                    None => "None".to_string()
-                });
-                let mut date_due: String = "".to_string();
-                if course.due_date.is_some() {
-                    match course.due_date.clone().unwrap().month {
-                        Some(month) => date_due.push_str(&month.to_string()),
-                        None => ()
-                    }
-                    date_due.push_str("-");
-                    match course.due_date.clone().unwrap().day {
-                        Some(day) => date_due.push_str(&day.to_string()),
-                        None => ()
-                    }
-                    date_due.push_str("-");
-                    match course.due_date.clone().unwrap().year {
-                        Some(year) => date_due.push_str(&year.to_string()),
-                        None => ()
-                    }
-                }
-                if course.due_time.is_some() {
-                    date_due.push_str(" ");
-                    match course.due_time.clone().unwrap().hours {
-                        Some(hours) => date_due.push_str(&hours.to_string()),
-                        None => ()
-                    }
-                    date_due.push_str(":");
-                    match course.due_time.clone().unwrap().minutes {
-                        Some(minutes) => date_due.push_str(&minutes.to_string()),
-                        None => ()
-                    }
-                }
-                println!("Due at {}", date_due);
-                if course.grade_category.is_some() {
-                    println!("{:#?}", course.grade_category.unwrap_or_default());
-                }
-                println!("{}", course.id.unwrap_or_default());
-                match course.materials {
-                    Some(materials) => {
-                        for material in materials {
-                            if material.form.is_some() && material.form.clone().unwrap().form_url.is_some() {
-                                let form_link = material.form.unwrap().form_url.unwrap();
-                                println!("form: {}", form_link);
-                            };
-                            if material.drive_file.clone().is_some() && material.drive_file.clone().unwrap().drive_file.is_some() {
-                                if material.drive_file.clone().unwrap().drive_file.unwrap().title.is_some() {
-                                    println!("{}", material.drive_file.clone().unwrap().drive_file.unwrap().title.unwrap());
-                                };
-                                if material.drive_file.clone().unwrap().drive_file.unwrap().alternate_link.is_some() {
-                                    println!("{}", material.drive_file.clone().unwrap().drive_file.unwrap().alternate_link.unwrap());
-                                };
-                            }
-                        }
-                    }
-                    None => println!("No mats")
-                }
-                if course.max_points.is_some() {
-                    println!("Points: {}", course.max_points.unwrap().to_string());
-                } else {
-                    println!("Not graded");
-                }
-                if course.multiple_choice_question.is_some() {
-                    for choice in course.multiple_choice_question.unwrap().choices.unwrap() {
-                        println!("{}", choice);
-                    }
-                }
-                if course.title.is_some() {
-                    println!("{}", course.title.unwrap());
-                }
-                if course.topic_id.is_some() {
-                    println!("Topic: {}", course.topic_id.unwrap());
-                }
-                if course.work_type.is_some() {
-                    println!("work_type: {}", course.work_type.unwrap());
-                }
-                println!("last updated: {:#?}", match course.update_time {
-                    Some(update_time) => update_time,
-                    None => course.creation_time.unwrap()
-                });
-                println!(" ");
-            }
-        }
-        let course_materials: (Response<Body>, ListCourseWorkMaterialResponse) = courses.course_work_materials_list(&the_id).doit().await.unwrap();
-        if course_materials.1.course_work_material.is_some() {
-            for course_mats in course_materials.1.course_work_material.unwrap() {
-                if course_mats.assignee_mode.is_none() {
-                    if course_mats.alternate_link.is_some() {
-                        println!("Link to work {}", course_mats.alternate_link.unwrap());
-                    } else {
-                        println!("No link");
-                    }
-                    println!("time made: {:#?}", course_mats.creation_time.unwrap());
-                    if course_mats.scheduled_time.is_some() {
-                        println!("time published: {}", course_mats.scheduled_time.unwrap());
-                    }
-                    println!("Author id: {}", match course_mats.creator_user_id {
-                        Some(creator_user_id) => creator_user_id,
-                        None => "Unknown Author id".to_string()
-                    });
-                    println!("Material: {}", match course_mats.description {
-                        Some(description) => description,
-                        None => "None".to_string()
-                    });
-                    if course_mats.id.is_some() {
-                        println!("Id: {}", course_mats.id.unwrap());
-                    }
-                    if course_mats.topic_id.is_some() {
-                        println!("In Topic Id: {}", course_mats.topic_id.unwrap());
-                    }
-                    println!("last updated: {:#?}", match course_mats.update_time {
-                        Some(update_time) => update_time,
-                        None => course_mats.creation_time.unwrap()
-                    });
-                    println!(" ");
-                }
-            }
-        }
-        let teachers: (Response<Body>, ListTeachersResponse) = courses.teachers_list(&the_id).doit().await.unwrap();
-        if teachers.1.teachers.is_some() {
-            context.insert("teachers", &teachers.1.teachers.clone().unwrap());
-        }
-        let topics: (Response<Body>, ListTopicResponse) = courses.topics_list(&the_id).doit().await.unwrap();
-        if topics.1.topic.is_some() {
-            context.insert("topics", &topics.1.topic.clone().unwrap());
-        }
-        //let course_work_student_submission_list: (Response<Body>, ListStudentSubmissionsResponse) = courses.course_work_student_submissions_list(course_id: &the_id).doit().await.unwrap();
-        println!("{:#?}", &context);
-        tera.render_to("course", &context, &mut buffer).unwrap();
-        let mut file = File::create(format!("html/courses/{}.html", the_id)).expect("Failed to create file");
-        file.write_all(&buffer).expect("Failed to write to file");
-        buffer = Vec::new();
     }
     
+    // Register the Tera filter function
+    tera.register_function("url_ok", move |args: &HashMap<String, Value>| {
+        if let Some(url) = args.get("url").and_then(|v| v.as_str()) {
+            let url = url.to_string(); // Clone the URL for async closure
+    
+            // Create a channel for communication between threads
+            let (sender, receiver) = mpsc::channel();
+    
+            // Spawn a new thread to run the async block
+            std::thread::spawn(move || {
+                let runtime = Runtime::new().unwrap();
+                let result = runtime.block_on(async move {
+                    check_url(url).await
+                });
+                let _ = sender.send(result);
+            });
+    
+            // Wait for the result from the spawned thread
+            let result = receiver.recv().unwrap();
+    
+            Ok(Value::Bool(result))
+        } else {
+            Err(tera::Error::msg("No 'url' argument provided"))
+        }
+    });
+
+    /*tera.register_function("to_utc", move |args: &HashMap<String, Value>| {
+        if let Some(date) = args.get("date") {
+            let utc_date = "".to_string();
+            match date.year {
+                Some(year) => date_due.push_str(&year.to_string()),
+                None => Err(tera::Error::msg("No 'date.month' argument"))
+            }
+            date_due.push_str("-");
+            match date.month {
+                Some(month) => date_due.push_str(&month.to_string()),
+                None => Err(tera::Error::msg("No 'date.month' argument"))
+            }
+            date_due.push_str("-");
+            match date.day {
+                Some(day) => date_due.push_str(&day.to_string()),
+                None => Err(tera::Error::msg("No 'date.day' argument"))
+            }
+    
+            Ok(Value::Bool(result))
+        } else {
+            Err(tera::Error::msg("No 'date or time' argument provided"))
+        }
+    });*/
+
+    file.write_all(&buffer).expect("Failed to write to file");
+    let mut reqquery_vec: Vec<CourseContent> = Vec::new();
+
+    for course in response.1.courses.unwrap() {
+        println!("Pulling Data From {}", course.name.clone().unwrap());
+        let start_time = Instant::now();
+        let the_id = course.clone().id.unwrap();
+        let course_content = CourseContent {
+            id: Some(course.clone().id.unwrap()),
+            course_announcements: Some(courses.announcements_list(&the_id).doit().await.unwrap().1.announcements.clone().unwrap_or_default()),
+            course_work: Some(courses.course_work_list(&the_id).doit().await.unwrap().1.course_work.clone().unwrap_or_default()),
+            course_materials: Some(courses.course_work_materials_list(&the_id).doit().await.unwrap().1.course_work_material.clone().unwrap_or_default()),
+            name: Some(course.name.clone().unwrap_or_default()),
+            teachers: Some(courses.teachers_list(&the_id).doit().await.unwrap().1.teachers.clone().unwrap_or_default()),
+            topics: Some(courses.topics_list(&the_id).doit().await.unwrap().1.topic.clone().unwrap_or_default()),
+            tera: tera.clone(),
+        };
+        reqquery_vec.push(course_content);
+        println!(
+            "Took {:?}",
+            start_time.elapsed(),
+        );
+    }
+
+    let tasks: Vec<_> = reqquery_vec.clone().iter().map(|course| {
+        let course = course.clone();
+        tokio::spawn(async move {
+            let start_time = Instant::now();
+            let mut buffer = Vec::new();
+            let mut context = Context::new();
+            context.insert("name", &course.name.clone().unwrap());
+            if course.course_announcements.is_some() {
+                context.insert("course_announcements", &course.course_announcements.clone().unwrap());
+            }
+            if course.course_work.is_some() {
+                context.insert("coursework", &course.course_work.clone().unwrap());
+            }
+            if course.course_materials.is_some() {
+                context.insert("course_materials", &course.course_materials.clone().unwrap());
+            }
+            if course.teachers.is_some() {
+                context.insert("teachers", &course.teachers.clone().unwrap());
+            }
+            if course.topics.is_some() {
+                context.insert("topics", &course.topics.clone().unwrap());
+            }
+            //let course_work_student_submission_list: (Response<Body>, ListStudentSubmissionsResponse) = courses.course_work_student_submissions_list(course_id: &the_id).doit().await.unwrap();
+            //println!("{:#?}", &context);
+            course.tera.render_to("course", &context, &mut buffer).unwrap();
+            let mut file = File::create(format!("html/courses/{}.html", course.clone().id.unwrap())).expect("Failed to create file");
+            file.write_all(&buffer).expect("Failed to write to file");
+            let end_time = Instant::now();
+            let iteration_duration = end_time - start_time;
+            println!("Course: {}, {}\nRender Time: {:?}", course.name.clone().unwrap(), course.clone().id.unwrap(), iteration_duration);
+        })
+    }).collect();
+    futures::future::join_all(tasks).await;
     Ok(())
 }
 
